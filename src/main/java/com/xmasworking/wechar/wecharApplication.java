16381cd766afa8a2f1208ea2f7b1e07410260ed6
Albert ZheYang <xmaspiano@Hotmail.com>
2018-10-09 09:45:36 +0800

    xmaspiano
    new project wechar service

---
+++ src/main/java/com/xmasworking/wechar/wecharApplication.java
@@ -0,0 +1,20 @@
+package com.xmasworking.wechar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author XmasPiano
+ * @date 2018/10/9 - 上午8:54
+ * Created by IntelliJ IDEA.
+ */
+@SpringBootApplication
+public class wecharApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(wecharApplication.class, args);
+    }
+
+}
